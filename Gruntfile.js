5cad5162493ff40828f8846719bd6e0b3450d779
Carmen V. Krol <carmen.krol@gmail.com>
2014-08-31 14:37:44 -0400

    hopefully this works

--- Gruntfile.js
+++ Gruntfile.js
@@ -416,6 +416,8 @@ module.exports = function (grunt) {
     ]);
   });
 
+  grunt.registerTask('heroku:production', 'clean, less, cssmin, uglify');
+
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
--- web.js
+++ web.js
@@ -4,4 +4,4 @@ var app = express();
  
 //app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(80);
+app.listen(process.env.PORT || 3000);
